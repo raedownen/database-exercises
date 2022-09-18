36e1a6d8d406f312f9f2bf15d230ef4c365c775b
Rachel Downen <rae.l.downen@gmail.com>
2022-09-18 10:14:34 -0500

    add # 3 from Andrews ex.

--- temporary_tables.sql
+++ temporary_tables.sql
@@ -55,14 +55,64 @@ everyone at the company. In order to make the comparison easier, you should use 
 In terms of salary, what is the best department right now to work for? The worst?
 Hint Consider that the following code will produce the z score for current salaries.
 -- Returns the historic z-scores for each salary
--- Notice that there are 2 separate scalar subqueries involved
-SELECT salary,
-    (salary - (SELECT AVG(salary) FROM salaries))
-    /
-    (SELECT stddev(salary) FROM salaries) AS zscore
-FROM salaries;
-*/
+-- Notice that there are 2 separate scalar subqueries involved*/
 
+select avg(salary), std(salary) from employees.salaries where to_date > now();
+create temporary table overall_aggregates as (
+    select avg(salary) as avg_salary, std(salary) as std_salary
+    from employees.salaries  where to_date > now()
+)
+select * from overall_aggregates;
+
+select dept_name, avg(salary) as department_current_average
+from employees.salaries
+join employees.dept_emp using(emp_no)
+join employees.departments using(dept_no)
+where employees.dept_emp.to_date > curdate()
+and employees.salaries.to_date > curdate()
+group by dept_name;
+
+drop table if exists current_info;
+
+# create the temp table using the query above
+create temporary table current_info as (
+    select dept_name, avg(salary) as department_current_average
+    from employees.salaries
+    join employees.dept_emp using(emp_no)
+    join employees.departments using(dept_no)
+    where employees.dept_emp.to_date > curdate()
+    and employees.salaries.to_date > curdate()
+    group by dept_name
+);
+
+select * from current_info;
+
+-- add on all the columns we'll end up needing:
+alter table current_info add overall_avg float(10,2);
+alter table current_info add overall_std float(10,2);
+alter table current_info add zscore float(10,2);
+
+# peek at the table again
+select * from current_info;
+
+-- set the avg and std
+update current_info set overall_avg = (select avg_salary from overall_aggregates);
+update current_info set overall_std = (select std_salary from overall_aggregates);
+
+
+
+-- update the zscore column to hold the calculated zscores
+update current_info 
+set zscore = (department_current_average - overall_avg) / overall_std;
+
+
+
+select * from andrew_king.current_info
+order by zscore desc;
+
+
+SELECT AVG(salary), STDDEV(salary)
+FROM employees.salaries;
 /*BONUS To your work with current salary zscores, determine the overall historic average departement 
 average salary, the historic overall average, and the historic zscores for salary. Do the zscores for 
 current department average salaries tell a similar or a different story than the historic department 
